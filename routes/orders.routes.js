f567bc0302ef9bcb04f7048078711bc6a3dcffa5
Alin Triff <triffalin@gmail.com>
2023-10-22 16:50:02 +0300

    saved orders in the database

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -5,5 +5,6 @@ const ordersController = require('../controllers/orders.controller');
 const router = express.Router();
 
 router.post('/', ordersController.addOrder);
+router.get('/', ordersController.getOrders);
 
 module.exports = router;
