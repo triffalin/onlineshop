f21ed0980abf372561efbc85c749d28c9ed7ba26
Alin Triff <triffalin@gmail.com>
2023-10-22 16:06:10 +0300

    added the order controller and a basic order model

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
 const adminRoutes = require('./routes/admin.routes');
 const cartRoutes = require('./routes/cart.routes');
+const orderRoutes = require('./routes/orders.routes');
 
 const app = express();
 
@@ -42,6 +43,7 @@ app.use(authRoutes);
 app.use(productsRoutes);
 app.use('/cart', cartRoutes);
 app.use(protectRoutesMiddleware);
+app.use('/orders', orderRoutes);
 app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
